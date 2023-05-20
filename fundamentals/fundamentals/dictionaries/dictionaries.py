a5dc62e78c47dedcec38946f89e9de99b948d9e7
nomisj <nomij5941@gmail.com>
2023-05-20 13:51:42 -0700

    commit

---
+++ fundamentals/fundamentals/dictionaries/dictionaries.py
@@ -0,0 +1,6 @@
+# Creating Dictionaries
+weekend = {"Sun": "Sunday", "Sat": "Saturday"} #literal notation
+capitals = {} #create an empty dictionary then add values
+capitals["svk"] = "Bratislava"
+capitals["deu"] = "Berlin"
+capitals["dnk"] = "Copenhagen"
