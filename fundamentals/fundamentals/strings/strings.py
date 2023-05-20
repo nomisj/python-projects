8ec25eb6d6db2492eab71329ebc11940c19f87c9
nomisj <nomij5941@gmail.com>
2023-05-19 17:27:45 -0700

    commit

---
+++ fundamentals/fundamentals/strings/strings.py
@@ -0,0 +1,7 @@
+# Strings are any sequence of charecters (letters, numerals, (/\#, etc)) enclosed in single or double quotes.
+print("this is a sample string")
+
+#Concatenating Strings and Variables with the print fuction
+# One of the first ways is by adding a comma after the string, followed by the variable. Note that the comma is outside the closing quotation mark of the string.
+name = "Nomi"
+print("May name is", name)
