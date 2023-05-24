d4404e15968d56eda8ae0a6a0f2e15d0c2a59aad
nomisj <nomij5941@gmail.com>
2023-05-24 11:53:00 -0700

    commit

--- fundamentals/fundamentals/dictionaries/dictionaries.py
+++ fundamentals/fundamentals/dictionaries/dictionaries.py
@@ -1,17 +1,19 @@
 # Creating Dictionaries
+# There are 3 types of sets in Python. Lists enclosed by [], tuples enclosed by (), and dictionaries enclosed by {}.
 weekend = {"Sun": "Sunday", "Sat": "Saturday"} #literal notation
 capitals = {} #create an empty dictionary then add values
 capitals["svk"] = "Bratislava"
 capitals["deu"] = "Berlin"
 capitals["dnk"] = "Copenhagen"
-# Using literal notation, the key-value pairs are enclosed by curly brackets. The pairs are seperated by commas. The "Sun" string is a key and the "Sunday" string is a value.
+# Above we created two dictionaries. Using literal notation, the key-value pairs are enclosed by curly brackets. The pairs are seperated by commas. The "Sun" string is a key and the "Sunday" string is a value.
+# Also by creating an empty dictionary and adding some values.
 # Each key in a dictionary must be unique. 
 
 # Accessing Values
 print(weekend["Sun"])
 print(capitals["svk"])
 
-# Removing Values
+# Removing Values - Two diffrent ways
 value_removed = capitals.pop('svk') # will remove the key 'svk' and return it's value
 del capitals['dnk'] # will delete the key, and not return anything
 
