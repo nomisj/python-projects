9acbe390cad2f916a14ca44230d13306042f1022
nomisj <nomij5941@gmail.com>
2023-05-29 20:48:58 -0700

    commit

--- fundamentals/extras/wk1_d1_lecture/notes.py
+++ fundamentals/extras/wk1_d1_lecture/notes.py
@@ -3,7 +3,7 @@ print("hello world")
 
 #########################
 
-# Variables
+# VARIABLES
 x = 5
 
 #Re-declaring a variable
@@ -11,20 +11,44 @@ x = "Hello there!"
 
 #########################
 
-# Lists
+# LISTS
 
 pets = ["Bolo", "Appa"]
 
-# How to add and remove from the list above
-pets.append("Squirt") # adds to a list
+# How to add and remove from the List above
+pets.append("Squirt") # adds to a List
 pets.insert(2, "Jamal") # the number says where to put it and then what
-# pets.pop() # removes from a list
+# pets.pop() # removes from a List
 pets.remove("Jamal") # needs the value of what you want removed
 
 #########################
 
-# Dictionary
+# DICTIONARIES
 owner = {"name": "Nomi", "age": 24, "location": "Monterey, CA"}
 
 # Print "Nomi is 24 years old and lives in Monterey, CA"
 print(f"{owner['name']} is {owner['age']} years old and lives in {owner['location']}.")
+
+# Printing the length of a Dictionary
+print("The length of the dictionary is:", len(owner))
+
+# How to print the entire Dictionary
+print("The entire dictionary is", owner)
+
+#########################
+
+# IF STATEMENTS
+
+# if x is = 10 create an if statement to print true if x is less than 5 and false if greater
+
+x = 10
+if x < 5:
+    print(True)
+else:
+    print(False)
+
+x = 5
+if x <= 5: # less than or equal to
+    print(True)
+else:
+    print(False)
