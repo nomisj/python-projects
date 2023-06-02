cf80f32af42eaeb7efdf2ffe24a27dba464cdbc1
nomisj <nomij5941@gmail.com>
2023-06-01 20:33:26 -0700

    commit

--- fundamentals/fundamentals/10. functions/functions_basic1/functions_basic_i.py
+++ fundamentals/fundamentals/10. functions/functions_basic1/functions_basic_i.py
@@ -1,135 +1,136 @@
-# #1
+#1
 def number_of_food_groups():
     return 5
-print(number_of_food_groups())
-#output 5
+print(number_of_food_groups()) 
+# ! >>> 5
 
-
-# #2
-# def number_of_military_branches():
-#     return 5
+#2
+def number_of_military_branches():
+    return 5
 # print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
-#output undefined
+# ! >>> undefined
 
-
-# #3
+#3
 def number_of_books_on_hold():
     return 5
-    return 10
+    return 10 #un-reachable code
 print(number_of_books_on_hold())
-#output is 5 first since it is the first return
+# ! >>> 5
 
-
-# #4
+#4
 def number_of_fingers():
     return 5
     print(10)
 print(number_of_fingers())
+# ! >>> 5
 
+#5
+def number_of_great_lakes():
+    print(5)
+x = number_of_great_lakes()
+print(x)
+# ! >>> 5 , None
 
-# #5
-# def number_of_great_lakes():
-#     print(5)
-# x = number_of_great_lakes()
-# print(x)
+#6
+def add(b,c):
+    print(b+c)
+print(add(1,2) + add(2,3))
+# ! >>> 3, 5,Error can't add NoneTypes
 
+#7
+def concatenate(b,c):
+    return str(b)+str(c)
+print(concatenate(2,5))
+# ! >>> "25"
 
-# #6
-# def add(b,c):
-#     print(b+c)
-# print(add(1,2) + add(2,3))
+#8
+def number_of_oceans_or_fingers_or_continents():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(number_of_oceans_or_fingers_or_continents())
+# ! >>> 100, 10
 
+#9
+def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+# ! >>> 7
+print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# ! >>> 14
+print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# ! >>> 21
 
-# #7
-# def concatenate(b,c):
-#     return str(b)+str(c)
-# print(concatenate(2,5))
+#10
+def addition(b,c):
+    return b+c
+    return 10
+print(addition(3,5))
+# ! >>> 8
 
+#11
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+print(b)
+foobar()
+print(b)
+# ! >>> 500,500, 300, 500
 
-# #8
-# def number_of_oceans_or_fingers_or_continents():
-#     b = 100
-#     print(b)
-#     if b < 10:
-#         return 5
-#     else:
-#         return 10
-#     return 7
-# print(number_of_oceans_or_fingers_or_continents())
+#12
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+foobar()
+print(b)
+# ! >>>> 500, 500, 300, 500
 
+#13
+b = 500
+print(b)
+def foobar():
+    b = 300
+    print(b)
+    return b
+print(b)
+b=foobar()
+print(b)
+# ! >>> 500, 500, 300, 300
 
-# #9
-# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
-#     if b<c:
-#         return 7
-#     else:
-#         return 14
-#     return 3
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+#14
+def foo():
+    print(1)
+    bar()
+    print(2)
+def bar():
+    print(3)
+foo()
+# ! >>> 1, 3, 2
 
+#15
+def foo():
+    print(1)
+    x = bar()
+    print(x)
+    return 10
+def bar():
+    print(3)
+    return 5
+y = foo()
+print(y)
 
-# #10
-# def addition(b,c):
-#     return b+c
-#     return 10
-# print(addition(3,5))
-
-
-# #11
-# b = 500
-# print(b)
-# def foobar():
-#     b = 300
-#     print(b)
-# print(b)
-# foobar()
-# print(b)
-
-
-# #12
-# b = 500
-# print(b)
-# def foobar():
-#     b = 300
-#     print(b)
-#     return b
-# print(b)
-# foobar()
-# print(b)
-
-
-# #13
-# b = 500
-# print(b)
-# def foobar():
-#     b = 300
-#     print(b)
-#     return b
-# print(b)
-# b=foobar()
-# print(b)
-
-
-# #14
-# def foo():
-#     print(1)
-#     bar()
-#     print(2)
-# def bar():
-#     print(3)
-# foo()
-
-
-# #15
-# def foo():
-#     print(1)
-#     x = bar()
-#     print(x)
-#     return 10
-# def bar():
-#     print(3)
-#     return 5
-# y = foo()
-# print(y)
+# ! >>> 1, 3, 5 , 10
